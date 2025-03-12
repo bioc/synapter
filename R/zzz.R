34e80f1ee54b32ff3636710e1e529d53676b5507
lshep <lori.shepherd@roswellpark.org>
2025-03-12 08:34:00 -0400

    User requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -2,6 +2,10 @@
   packageStartupMessage(paste0("This is the 'synapter' version ",
                                packageVersion("synapter"), ".\n",
                               "Read '?synapter' to get started.\n", sep=""))
+  msg <- sprintf(
+      "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.22")
+  .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
   addVigs2WinMenu("synapter")
 }
 
